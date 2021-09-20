52052f0c4327b35440c70b31dabfd9dc5aee71b3
David <davidreyesizagui@gmail.com>
2021-09-20 12:17:06 -0600

    delete and put

--- src/main/java/com/example/demo/AzureDavidApplication.java
+++ src/main/java/com/example/demo/AzureDavidApplication.java
@@ -6,8 +6,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -30,6 +33,26 @@ public class AzureDavidApplication {
 		return repository.save(employee);
 		
 	}
+	
+	@PutMapping("/employee/{id}")
+	public Employee editEmployee(@RequestBody Employee employee,@PathVariable Integer id) {
+		Employee empleadoActual = repository.findById(id).orElse(null);
+		empleadoActual.setName(employee.getName());
+		empleadoActual.setDept(employee.getDept());
+		empleadoActual.setSalary(employee.getSalary());
+		Employee empleadoUpdated = null;
+
+		empleadoUpdated = repository.save(empleadoActual);
+	
+		return empleadoUpdated;
+	}
+	
+	@DeleteMapping("/employee/{id}")
+	public void editEmployee(@PathVariable Integer id) {
+		repository.deleteById(id);
+		
+	}
+
 	
 	
 	
